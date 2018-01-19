e9ea43bc58124eb2d69cba6d7e01f4d6a0bfa713
Richard Chun <richard.chun91@gmail.com>
2018-01-19 17:30:44 -0500

    Animation added when switching between components and fetching data from server

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { flyInOut } from '../animations/app.animation';
+import { expand } from '../animations/app.animation';
+
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+  flyInOut(),
+  expand()
+  ]
 })
 export class AboutComponent implements OnInit {
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,10 +5,21 @@ import { DishService} from '../services/dish.service';
 import { Promotion } from '../shared/promotion';
 import { PromotionService } from '../services/promotion.service';
 
+import { flyInOut } from '../animations/app.animation';
+import { expand } from '../animations/app.animation';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+  flyInOut(),
+  expand()
+  ]
 })
 export class HomeComponent implements OnInit {
 
