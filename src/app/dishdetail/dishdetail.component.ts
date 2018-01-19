46360d57699ec52ca24559f20beacd6182b5e2d3
Richard Chun <richard.chun91@gmail.com>
2018-01-18 19:31:01 -0500

    Animation added when setting next/prev dish

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import { Dish } from '../shared/dish';
 import { Comment } from '../shared/comment';
@@ -12,7 +13,20 @@ import 'rxjs/add/operator/switchMap';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -24,6 +38,7 @@ export class DishdetailComponent implements OnInit {
   next: number;
   errorMsg: string;
   comment: Comment;
+  visibility = 'shown';
 
   formErrors = {
     'author': '',
@@ -54,8 +69,11 @@ export class DishdetailComponent implements OnInit {
 
       this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
       this.route.params
-        .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-        .subscribe(dish => { this.dish = dish; this.dishcopy = dish;  this.setPrevNext(dish.id); },
+        .switchMap((params: Params) => {
+          this.visibility = 'hidden';
+          return this.dishservice.getDish(+params['id']);
+        })
+        .subscribe(dish => { this.dish = dish; this.dishcopy = dish;  this.setPrevNext(dish.id); this.visibility = 'shown';  },
         errormsg => this.errorMsg = <any>errormsg);
     }
 
